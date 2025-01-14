3f5350b973a09aac006bbf9bcc25b69196af8d2b
agaonsindhe <anshuman.gaonsindhe@gmail.com>
2025-01-14 15:45:26 +0530

    final commit 1

--- backend/test/test_model_loader.py
+++ backend/test/test_model_loader.py
@@ -5,7 +5,7 @@ def test_load_model():
     """
     Test if the model and tokenizer load correctly.
     """
-    model_name = ""
+    model_name = "agaonsindhe/grammar-error-correction-c2400m-t5-base"
     tokenizer, model = load_model(model_name)
     assert tokenizer is not None
     assert model is not None
